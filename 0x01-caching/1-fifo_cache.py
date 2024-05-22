0cfea45940a03114b38c2c31fe6b42e2d5436ae7
wallace <exnesswallace@gmail.com>
2024-05-22 11:35:29 +0000

    forgot to cal the parent init

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,9 +8,17 @@ BaseCaching = __import__('base_caching').BaseCaching
 class FIFOCache(BaseCaching):
     """FIFOCache is a caching system"""
 
+    def __init__(self):
+        """Initialize the FIFOCache"""
+        super().__init__()
+
+
     def put(self, key, item):
         """Add an item in the cache, and removes the first
         pair in the dict and prints the the removed key"""
+        if key is None or item is None:
+            return
+
         value = self.cache_data.values()
         value_length = len(value)
 
@@ -18,9 +26,6 @@ class FIFOCache(BaseCaching):
             first_key = next(iter(self.cache_data))
             self.cache_data.pop(first_key)
             print("Discard: {}".format(first_key))
-
-        if key is None or item is None:
-            return
 
         self.cache_data[key] = item
 
