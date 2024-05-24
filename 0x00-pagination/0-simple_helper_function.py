013881c29220abaa85d316eaf7deac0d7e1a7805
wallace <exnesswallace@gmail.com>
2024-05-24 13:35:21 +0000

    added the type of a variable

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,5 +10,5 @@ def index_range(page: int, page_size: int) -> Tuple:
 
     start_index: int = (page - 1) * page_size
     end_index: int = start_index + page_size
-    indexes = (start_index, end_index)
+    indexes: Tiple(int) = (start_index, end_index)
     return (indexes)
