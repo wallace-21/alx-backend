6346f456b020ea552a4663c64f16651cec533b28
wallace <exnesswallace@gmail.com>
2024-05-24 13:30:09 +0000

    function that returns a start and end index of a page

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""import typing to set types"""
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """ unction, you need to calculate the start and
+    end indexes for the given page and page_size"""
+
+    start_index: int = (page - 1) * page_size
+    end_index: int = start_index + page_size
+    indexes = (start_index, end_index)
+    return (indexes)
