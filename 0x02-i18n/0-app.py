8541505811a27d27ffa52fe0af0d356768c4e65f
root <root@Wallace.localdomain>
2024-06-04 14:47:04 +0200

    a basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
+"""import flask module"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    """fucntion that renders a template for a flask app"""
+    return render_template("0-index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
